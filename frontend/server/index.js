7fc3e9be4eb0ba5feb9e6becd1b8881e0370ca65
redhizon <80209772+redhizon@users.noreply.github.com>
2022-06-08 04:49:50 +0800

    Update index.js
    Update the comment for app.listen()

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -30,7 +30,6 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-// start express server on port 5000
 // start server
 const port = 5000;
 app.listen(port, myIP, (err) => {
